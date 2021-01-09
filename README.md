be7e076aae2a231665436f787728623cd627cb93
MShawon <shawonk440@gmail.com>
2021-01-09 07:18:14 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+![ViewCount](https://views.whatilearened.today/views/github/MShawon/YouTube-viewer.svg)
+
+    Yb  dP  dP"Yb  88   88 888888 88   88 88""Yb 888888 
+     YbdP  dP   Yb 88   88   88   88   88 88__dP 88__   
+      8P   Yb   dP Y8   8P   88   Y8   8P 88""Yb 88""   
+     dP     YbodP  `YbodP'   88   `YbodP' 88oodP 888888 
+
+                         Yb    dP 88 888888 Yb        dP 888888 88""Yb 
+                          Yb  dP  88 88__    Yb  db  dP  88__   88__dP 
+                           YbdP   88 88""     YbdPYbdP   88""   88"Yb  
+                            YP    88 888888    YP  YP    888888 88  Yb
+
+# YouTube Viewer
+Simple program to increase YouTube views written in Python.
+**Discalimer:** This has been developed for educational purposes only.
+# Requirements
+ * Python 3.x
+ * High speed Internet Connection
+ * Good proxy list
+  
+# Installation 
+ Open command prompt and type
+ ```bash
+ $ git clone https://github.com/MShawon/Numerical-Method.git
+
+ $ cd YouTube-Viewer
+
+ $ pip install -r requirements.txt
+ ```
+## Important
+Check your chrome browser version and download same versions chromedriver.exe from https://chromedriver.chromium.org/downloads here and place it in the YouTube-Viewer folder.
+
+# Usage
+ * Put youtube video links in the urls.txt. For multiple videos place urls in multple lines. To find video link in YouTube click share and copy.
+ * Open command prompt in YouTube-Viewer folder and type
+    ```bash
+    $ python youtube_viewer.py
+    ```
+   * Enter amount of views you want.
+   * Input proxy list or let program to automatic handle proxies.
+
+ ![alt text](demo.png "Demo")
---
+++ youtube_viewer.py
@@ -0,0 +1,227 @@
+import concurrent.futures.thread
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import os
+import random
+import time
+
+import requests
+from fake_useragent import UserAgent
+from fake_useragent import UserAgentError
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+os.system("")
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+print(bcolors.OKGREEN + """
+
+Yb  dP  dP"Yb  88   88 888888 88   88 88""Yb 888888                   
+ YbdP  dP   Yb 88   88   88   88   88 88__dP 88__                     
+  8P   Yb   dP Y8   8P   88   Y8   8P 88""Yb 88""                     
+ dP     YbodP  `YbodP'   88   `YbodP' 88oodP 888888  
+
+                        Yb    dP 88 888888 Yb        dP 888888 88""Yb 
+                         Yb  dP  88 88__    Yb  db  dP  88__   88__dP 
+                          YbdP   88 88""     YbdPYbdP   88""   88"Yb  
+                           YP    88 888888    YP  YP    888888 88  Yb 
+""" + bcolors.ENDC)
+print(bcolors.OKCYAN + """
+           [ GitHub : https://github.com/MShawon/YouTube-Viewer ]
+""" + bcolors.ENDC)
+
+print(bcolors.WARNING +'Collecting User-Agent...'+ bcolors.ENDC)
+try:
+    ua = UserAgent(use_cache_server=False, verify_ssl=False)
+except UserAgentError:
+    agent_link = 'https://gist.githubusercontent.com/pzb/b4b6f57144aea7827ae4/raw/cf847b76a142955b1410c8bcef3aabe221a63db1/user-agents.txt'
+    response = requests.get(agent_link).content
+    ua = response.decode().split('\n')
+    ua = list(filter(None, ua))
+
+PROXY = None
+driver = None
+status = None
+
+urls = []
+proxy_list = []
+view = []
+
+
+def load_url():
+    global urls
+    print(bcolors.WARNING + 'Loading urls...'+ bcolors.ENDC)
+    filename = 'urls.txt'
+    load = open(filename)
+    loaded = [items.rstrip().strip() for items in load]
+    load.close()
+
+    for lines in loaded:
+        urls.append(lines)
+
+    print(bcolors.OKGREEN +
+          '{} url loaded from urls.txt'.format(len(urls)) + bcolors.ENDC)
+    return urls
+
+
+def gather_proxy():
+    global proxy_list
+    print(bcolors.OKGREEN + 'Scraping proxies ...' + bcolors.ENDC)
+
+    link_list = ['https://www.proxyscan.io/download?type=http',
+                 'https://www.proxyscan.io/download?type=https',
+                 'https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt',
+                 'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt',
+                 'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/proxy.txt']
+
+    for link in link_list:
+        response = requests.get(link)
+        output = response.content.decode()
+        proxy = output.split('\n')
+        proxy_list = proxy_list + proxy
+        print(bcolors.OKGREEN +
+              '{} proxies gathered from {}'.format(len(proxy), link) + bcolors.ENDC)
+
+    return proxy_list
+
+
+def load_proxy():
+    global proxy_list
+
+    filename = input(bcolors.OKBLUE +
+                     'Enter your proxy file name: ' + bcolors.ENDC)
+    load = open(filename)
+    loaded = [items.rstrip().strip() for items in load]
+    load.close()
+
+    for lines in loaded:
+        proxy_list.append(lines)
+
+    return proxy_list
+
+
+def viewVideo(position):
+    try:
+        PROXY = proxy_list[position]
+
+        proxyDict = {
+            "http": "http://"+PROXY,
+            "https": "https://"+PROXY,
+            "ftp": "ftp://"+PROXY,
+        }
+
+        response = requests.get('https://www.youtube.com/', proxies=proxyDict)
+        status = response.status_code
+
+        if status == 200:
+            try:
+                url = random.choice(urls)
+                options = webdriver.ChromeOptions()
+                options.add_experimental_option(
+                    'excludeSwitches', ['enable-logging'])
+                options.headless = True
+                options.add_argument("--log-level=3")
+                options.add_argument(f"user-agent={ua.random}")
+                webdriver.DesiredCapabilities.CHROME['loggingPrefs'] = {
+                    'driver': 'OFF', 'server': 'OFF', 'browser': 'OFF'}
+                webdriver.DesiredCapabilities.CHROME['proxy'] = {
+                    "httpProxy": PROXY,
+                    "ftpProxy": PROXY,
+                    "sslProxy": PROXY,
+
+                    "proxyType": "MANUAL",
+                }
+
+                driver = webdriver.Chrome(
+                    executable_path="chromedriver.exe", options=options)
+
+                driver.get(url)
+                time.sleep(20)
+
+                mute = driver.find_element_by_css_selector(
+                    'button.ytp-mute-button.ytp-button')
+                mute.send_keys(Keys.ENTER)
+                
+                time.sleep(5)
+                video_len = driver.execute_script(
+                    "return document.getElementById('movie_player').getDuration()")
+
+                print(bcolors.OKBLUE + "Tried {} |".format(position) + bcolors.OKGREEN +
+                      ' {} --> Video Found : {} | Duration : {:.3f} seconds '.format(PROXY, url, int(video_len)/60) + bcolors.ENDC)
+
+                video_len = video_len - 0.5  #Removing 30 seconds from total duration 
+                time.sleep(video_len)
+                driver.quit()
+
+                view.append(position)
+                print(bcolors.OKCYAN +
+                      'View added : {}'.format(len(view)) + bcolors.ENDC)
+                status = 400
+
+            except Exception as e:
+                print(bcolors.FAIL + f"Tried {position} |" + e + bcolors.ENDC)
+                driver.quit()
+                status = 400
+                pass
+
+    except:
+        proxy_list.remove(PROXY)
+        print(bcolors.OKBLUE + f"Tried {position} |" + bcolors.FAIL +
+              ' {} --> Bad proxy removed from proxy list'.format(PROXY) + bcolors.ENDC)
+        pass
+
+def main():
+    pool_number = [i+1 for i in range(total_proxies)]
+
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+        futures = [executor.submit(viewVideo, position) for position in pool_number]
+
+        try:
+            for future in as_completed(futures):
+                future.result()
+        except KeyboardInterrupt:
+            executor._threads.clear()
+            concurrent.futures.thread._threads_queues.clear()
+            
+
+if __name__ == '__main__':
+    load_url()
+    views = int(input(bcolors.OKBLUE + 'Amount of views : ' + bcolors.ENDC))
+
+    threads = int(
+        input(bcolors.OKBLUE+'Threads (recommended = 10): ' + bcolors.ENDC))
+
+    handle_proxy = str(input(
+        bcolors.OKBLUE + 'Let YouTube Viewer handle proxies ? [Y/n] : ' + bcolors.ENDC)).lower()
+
+    if handle_proxy == 'y' or handle_proxy == 'yes' or handle_proxy == '':
+        gather_proxy()
+    else:
+        load_proxy()
+
+    proxy_list = list(set(proxy_list))
+    total_proxies = len(proxy_list)
+    print(bcolors.OKCYAN + 'Total proxies : {}'.format(total_proxies) + bcolors.ENDC)
+
+    check = 0
+    while len(view)<views:
+        try:
+            check += 1
+            if check == 1:
+                main()
+            else:
+                gather_proxy()
+                main()
+        except KeyboardInterrupt:
+            exit()
